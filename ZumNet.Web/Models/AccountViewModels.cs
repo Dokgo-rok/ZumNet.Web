b010f3a63be07a4a97d2eb450a67c5b85859f399
BYRL <byeongrok.lee@gmail.com>
2021-07-14 17:18:19 +0900

    언어설정

--- ZumNet.Web/Models/AccountViewModels.cs
+++ ZumNet.Web/Models/AccountViewModels.cs
@@ -6,16 +6,16 @@ namespace ZumNet.Web.Models
     public class AccountViewModels
     {
         [Required]
-        [Display(Name = "로그인 아이디")]
+        [Display(Name = "lbl_LoginId", ResourceType = typeof(Resources.Global))]
         [DataType(DataType.Text)]
         public string LoginId { get; set; }
 
         [Required]
-        [Display(Name = "비밀번호")]
+        [Display(Name = "lbl_Password", ResourceType = typeof(Resources.Global))]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Display(Name = "아이디 저장")]
+        [Display(Name = "lbl_RememberMe", ResourceType = typeof(Resources.Global))]
         public bool RememberMe { get; set; }
     }
 }
