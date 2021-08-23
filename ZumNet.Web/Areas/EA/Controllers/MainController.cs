dc0565f495029d59952a877fc2647f09f5a67ceb
BYRL <byeongrok.lee@gmail.com>
2021-08-23 20:23:49 +0900

    결재 메인

--- ZumNet.Web/Areas/EA/Controllers/MainController.cs
+++ ZumNet.Web/Areas/EA/Controllers/MainController.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Newtonsoft.Json.Linq;
+using ZumNet.Framework.Util;
+
+using ZumNet.Web.Bc;
+using ZumNet.Web.Filter;
+
 namespace ZumNet.Web.Areas.EA.Controllers
 {
     public class MainController : Controller
     {
         // GET: EA/Main
+        [SessionExpireFilter]
+        [Authorize]
         public ActionResult Index()
         {
             string rt = Bc.CtrlHandler.PageInit(this, false);
+            if (rt != "")
+            {
+                return View("~/Views/Shared/_Error.cshtml", new HandleErrorInfo(new Exception(rt), this.RouteData.Values["controller"].ToString(), this.RouteData.Values["action"].ToString()));
+            }
+
+            rt = "잘못된 경로로 접근했습니다!!";
+            if (ViewBag.R == null || ViewBag.R.ct == null || ViewBag.R.ct == "0")
+            {
+                return View("~/Views/Shared/_Error.cshtml", new HandleErrorInfo(new Exception(rt), this.RouteData.Values["controller"].ToString(), this.RouteData.Values["action"].ToString()));
+            }
 
             return View();
         }
---
+++ ZumNet.Web/Scripts/EA/ea.js
@@ -0,0 +1,14 @@
+﻿//전자결재 메인, 리스트뷰
+
+$(function () {
+
+    $('#__LeftMenu .sidenav-toggle').click(function (e) {
+        e.preventDefault();
+        
+        $(this).next().slideToggle(300, function() {
+            $(this).parent().toggleClass('open');
+        });
+    });
+    
+
+});
