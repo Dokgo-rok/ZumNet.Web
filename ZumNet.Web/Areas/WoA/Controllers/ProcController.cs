2155d3a6c903f9552e537fda323a0e6f3f6933ee
Jihun Choi <neostyx@gmail.com>
2021-09-08 00:27:33 +0900

    프로세스 관리 로직 추가

--- ZumNet.Web/Areas/WoA/Controllers/ProcController.cs
+++ ZumNet.Web/Areas/WoA/Controllers/ProcController.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZumNet.BSL.FlowBiz;
+using ZumNet.Framework.Core;
+using ZumNet.Framework.Entities.Web;
+using ZumNet.Framework.Util;
 
 namespace ZumNet.Web.Areas.WoA.Controllers
 {
@@ -11,6 +17,16 @@ namespace ZumNet.Web.Areas.WoA.Controllers
         // GET: WoA/Proc
         public ActionResult Index()
         {
+            int domainID = StringHelper.SafeInt(Session["DNID"].ToString());
+
+            using (EApprovalBiz eApprovalBiz = new EApprovalBiz())
+            {
+                // 프로세스 조회
+                ServiceResult result = eApprovalBiz.SelectProcessListByCondition(domainID, 0, "Y");
+
+                ViewData["processListJson"] = JsonConvert.SerializeObject(result.ResultDataTable);
+            }
+
             return View();
         }
     }
